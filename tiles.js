3efb546cbefe32fb0fdc1e46bd876d0b2769a723
ditam <ditam.github@gmail.com>
2019-04-29 18:44:49 +0200

    add translation from mouse coords to iso tile, support for hover and select

--- tiles.js
+++ tiles.js
@@ -6,7 +6,9 @@ const PARAMS = Object.freeze({
   OFFSET_Y: 50,
   TILE_WIDTH: 150,
   ASPECT: 150/108,
-  GRID_COLOR: 0xcccccc
+  GRID_COLOR: 0xcccccc,
+  HOVER_COLOR: 0xcccc00,
+  SELECT_COLOR: 0xdd3333
 });
 
 // NB: when adding size adjustment values, derive from TILE_WIDTH
@@ -28,7 +30,7 @@ const app = new PIXI.Application({
   antialias: true
 });
 document.body.appendChild(app.view);
-const graphics = new PIXI.Graphics();
+const gridlines = new PIXI.Graphics();
 const loader = PIXI.loader;
 
 for (let key in objects) {
@@ -49,35 +51,41 @@ const map = {};
 })();
 
 (function drawGrid() {
-  graphics.lineStyle(1.5, PARAMS.GRID_COLOR);
+  gridlines.lineStyle(1.5, PARAMS.GRID_COLOR);
   const x0 = PARAMS.OFFSET_X;
   const y0 = PARAMS.OFFSET_Y;
   const tileW = PARAMS.TILE_WIDTH;
   const tileH = PARAMS.TILE_WIDTH/PARAMS.ASPECT;
   for (let i = 0;i<PARAMS.MAP_ROWS+1; i++) {
-    graphics.moveTo(
+    gridlines.moveTo(
       x0 + 0.5*tileW - i*tileW/2,
       y0 + i*tileH/2
     );
-    graphics.lineTo(
+    gridlines.lineTo(
       x0 + 0.5*tileW + PARAMS.MAP_COLS * tileW/2  - i*tileW/2,
       y0 + PARAMS.MAP_COLS * tileH/2 + i*tileH/2
     );
   }
   for (let j = 0;j<PARAMS.MAP_COLS+1; j++) {
-    graphics.moveTo(
+    gridlines.moveTo(
       x0 + 0.5*tileW + j*tileW/2,
       y0 + j*tileH/2
     );
-    graphics.lineTo(
+    gridlines.lineTo(
       x0 + 0.5*tileW - PARAMS.MAP_ROWS * tileW/2 + j*tileW/2,
       y0 + PARAMS.MAP_ROWS * tileH/2 + j*tileH/2
     );
   }
 })();
 
+let mouseX = 0;
+let mouseY = 0;
+
 loader.load((loader, resources) => {
-  const thing = new PIXI.Sprite(resources.thing.texture);
+  document.addEventListener('mousemove', function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
 
   for (let i=0; i<PARAMS.MAP_ROWS; i++) {
     for (let j=0; j<PARAMS.MAP_COLS; j++) {
@@ -94,16 +102,99 @@ loader.load((loader, resources) => {
     }
   }
 
-  thing.width = PARAMS.TILE_WIDTH;
-  thing.x = 300;
-  thing.y = 85;
+  const x0 = PARAMS.OFFSET_X;
+  const y0 = PARAMS.OFFSET_Y;
+  const tileW = PARAMS.TILE_WIDTH;
+  const tileH = PARAMS.TILE_WIDTH/PARAMS.ASPECT;
 
-  app.stage.addChild(graphics);
+  const hoverMarker = new PIXI.Graphics();
+  hoverMarker.moveTo(x0+tileW/2, y0);
+  hoverMarker.lineStyle(4, PARAMS.HOVER_COLOR);
+  hoverMarker.lineTo(x0+tileW, y0+tileH/2);
+  hoverMarker.lineTo(x0+tileW/2, y0+tileH);
+  hoverMarker.lineTo(x0, y0+tileH/2);
+  hoverMarker.lineTo(x0+tileW/2, y0);
 
-  app.stage.addChild(thing);
+  const selectionMarker = new PIXI.Graphics();
+  selectionMarker.moveTo(x0+tileW/2, y0);
+  selectionMarker.lineStyle(4, PARAMS.SELECT_COLOR);
+  selectionMarker.lineTo(x0+tileW, y0+tileH/2);
+  selectionMarker.lineTo(x0+tileW/2, y0+tileH);
+  selectionMarker.lineTo(x0, y0+tileH/2);
+  selectionMarker.lineTo(x0+tileW/2, y0);
 
-  // Listen for frame updates
+  app.stage.addChild(gridlines);
+  app.stage.addChild(hoverMarker);
+  app.stage.addChild(selectionMarker);
+
   app.ticker.add(() => {
-    // dummy
+    const hoveredTile = getTileFromCoords(mouseX, mouseY);
+    if (hoveredTile) {
+      const tileX = - hoveredTile.row*tileW/2 + hoveredTile.col*tileW/2;
+      const tileY = hoveredTile.row*tileH/2 + hoveredTile.col*tileH/2;
+      hoverMarker.x = tileX;
+      hoverMarker.y = tileY;
+    }
+  });
+
+  document.addEventListener('click', function(e) {
+    const clickedTile = getTileFromCoords(e.clientX, e.clientY);
+    if (clickedTile) {
+      const tileX = - clickedTile.row*tileW/2 + clickedTile.col*tileW/2;
+      const tileY = clickedTile.row*tileH/2 + clickedTile.col*tileH/2;
+      selectionMarker.x = tileX;
+      selectionMarker.y = tileY;
+      selectionMarker.visible = true;
+    } else {
+      selectionMarker.visible = false;
+    }
   });
 });
+
+function getTileFromCoords(clientX, clientY) {
+  // We calculate the hit tile directly from the coordinates, returning undefined if no tile is hit.
+  // This is done by translating (x,y) into the isometric coordinate system (projecting to the axes),
+  // then using simple division by the known tile sizes to get the row and column values directly.
+
+  // To follow the logic of this function, consider the triangle ABC where A is (offsetX, offsetY), B is (x, y),
+  // and C is the sought projection of B onto the isometric axis (for simplicity, in the clientX < offsetX case):
+  // - The isometric axis tilt alpha is known from the tile sizing params
+  // - AB distance is known
+  // - Angle CAB can be calculated by projecting B onto the horizontal line of A and forming the right triangle ABB'
+  // - Note that angle BCA is 2*alpha (bisect with a horizontal line to see)
+  // - With CAB, BCA and AB known, the law of sines yields the missing sides of the ABC triangle.
+  // (Where AC is the sought projection to the left axis and CB is the projection to the right axis.)
+  const x = clientX - PARAMS.OFFSET_X - PARAMS.TILE_WIDTH/2;
+  const y = clientY - PARAMS.OFFSET_Y;
+  const tileW = PARAMS.TILE_WIDTH;
+  const tileH = PARAMS.TILE_WIDTH/PARAMS.ASPECT;
+  const tileEdge = Math.sqrt((tileW/2)*(tileW/2)+(tileH/2)*(tileH/2));
+  const axisTilt = Math.asin((tileH/2)/tileEdge);
+  const AB = Math.sqrt(x*x+y*y);
+  const ABangleFromHorizontal = Math.asin(y/AB);
+
+  if (ABangleFromHorizontal < axisTilt) return; // surely no hovered tile
+
+  const CAB = ABangleFromHorizontal - axisTilt;
+  const BCA = 2*axisTilt;
+  const ABC = Math.PI - CAB - BCA;
+
+  // applying the law of sines to ABC:
+  let projectionY = AB / Math.sin(BCA) * Math.sin(ABC);
+  let projectionX = AB / Math.sin(BCA) * Math.sin(CAB);
+
+  // Until this point, we assumed that clientX < offsetX, but if it was not the case, the math results in switched numbers
+  if (x >= 0) [projectionY, projectionX] = [projectionX, projectionY];
+
+  const hoveredRow = Math.floor(projectionY/tileEdge);
+  const hoveredCol = Math.floor(projectionX/tileEdge);
+
+  // clamp to actual grid size:
+  if (hoveredRow >= PARAMS.MAP_ROWS) return;
+  if (hoveredCol >= PARAMS.MAP_COLS) return;
+
+  return {
+    row: hoveredRow,
+    col: hoveredCol
+  };
+}
